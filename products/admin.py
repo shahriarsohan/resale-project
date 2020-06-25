3a9d440b659ef26e898efca5a11435fae7255691
shahriarsohan <sohanmock@gmail.com>
2020-06-26 00:51:42 +0600

    products and category model added and also requiremnets file updated

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Products, Category
+
+admin.site.register(Products)
+admin.site.register(Category)
--- products/models.py
+++ products/models.py
@@ -2,5 +2,38 @@ import os
 import random
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.core.validators import RegexValidator
 
 User = get_user_model()
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
+
+
+class Products(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.OneToOneField(
+        Category, on_delete=models.DO_NOTHING, null=True)
+    title = models.CharField(max_length=30)
+    description = models.TextField(max_length=255)
+    slug = models.SlugField(blank=True, null=True)
+    image = models.ImageField(blank=True, null=True)
+    phone_regex = RegexValidator(
+        regex='^(\+\d{1,3})?,?\s?\d{8,13}', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
+    phone_number = models.CharField(
+        validators=[phone_regex], max_length=17, blank=True)
+
+    used = models.BooleanField(default=False)
+    used_duration = models.PositiveIntegerField(blank=True, null=True)
+    division = models.CharField(max_length=10, blank=True, null=True)
+    zills = models.CharField(max_length=10, blank=True, null=True)
+    thana = models.CharField(max_length=10, blank=True, null=True)
+    zip_code = models.IntegerField()
+    is_solf = models.BooleanField(default=False, blank=True, null=True)
+
+    def __str__(self):
+        return self.title
